8d332731d305d89869d2ad85e2ede1078482ad40
sinofgreed <misha415@list.ru>
2025-05-01 20:51:27 +0500

    add: hidden constants

---
+++ src/main/java/com/github/Books_store/hiddenConstants.java
@@ -0,0 +1,11 @@
+package com.github.Books_store;
+
+public class hiddenConstants {
+    public static final String BOT_TOKEN = "hehe";
+
+    public static final String DBHOST = "localhost";
+    public static final String DBPORT = "3306";
+    public static final String DBUSER = "root";
+    public static final String DBPASS = "12345";
+    public static final String DBNAME = "tablestore";
+}
