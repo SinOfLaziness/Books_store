2d84d8b898e3abbb50d17b5d3896641a6fa1594f
sinofgreed <misha415@list.ru>
2025-05-01 21:35:53 +0500

    refactor: change gitignore

---
+++ src/main/java/com/github/Books_store/hiddenConstants.java
@@ -0,0 +1,11 @@
+package com.github.Books_store;
+
+public class hiddenConstants {
+    public static final String BOT_TOKEN = "7910001494:AAFpR5xo-QWM7K5nNO2X8AUgLJVfj04E3iY";
+
+    public static final String DBHOST = "localhost";
+    public static final String DBPORT = "3306";
+    public static final String DBUSER = "root";
+    public static final String DBPASS = "12345";
+    public static final String DBNAME = "tablestore";
+}
